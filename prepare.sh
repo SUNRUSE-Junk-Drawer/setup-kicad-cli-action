23cce4cf1d985b851d252ad113dd69a1d5e38d6b
jameswilddev <jameswilddev@outlook.com>
2021-03-07 10:54:32 +0000

    Replace script.

--- prepare.sh
+++ prepare.sh
@@ -14,11 +14,9 @@ echo ::set-output name=sha::$(git rev-parse temp)
 if [ "$(uname)" == "Darwin" ]; then
   echo TODO
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  cd scripting/build_tools
-  chmod +x get_libngspice_so.sh
-  ./get_libngspice_so.sh
-  sudo ./get_libngspice_so.sh install
-  cd ../..
+  wget --output-document=get_libngspice_so.sh https://gitlab.com/kicad/code/kicad/-/raw/master/scripting/build_tools/get_libngspice_so.sh?inline=false
+
+  ./get_libngspice_so.sh install
 
   sudo apt-get install ninja-build libglew-dev libglm-dev libcurl4-openssl-dev libboost-dev -y
 else
